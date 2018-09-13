c7ac61f1ec06dc9cf4898076df5075d2c4c7a68b
Chris Pritchard <chris.pritchard@edpay.nz>
2018-09-14 10:57:52 +1200

    Implemented a solution printer

--- Program.fs
+++ Program.fs
@@ -6,22 +6,23 @@ let main _ =
             [' ';' ';' ';' ';' ';' ';' ';' ';' ';' ']
             [' ';' ';' ';' ';' ';' ';' ';' ';' ';' ']
             [' ';' ';' ';' ';' ';' ';' ';' ';' ';' ']
-            [' ';' ';' ';' ';' ';' ';' ';' ';' ';' ']
+            [' ';' ';' ';' ';' ';' ';' ';'X';'X';' ']
+            [' ';' ';' ';' ';' ';' ';'X';'X';' ';' ']
+            [' ';' ';' ';' ';' ';' ';'X';' ';' ';' ']
+            [' ';' ';' ';' ';'X';'X';'X';' ';'X';'X']
+            [' ';' ';' ';' ';'X';' ';' ';' ';'X';' ']
+            [' ';' ';' ';' ';'X';' ';'X';'X';'X';' ']
+            [' ';' ';' ';' ';'X';' ';'X';' ';' ';' ']
+            [' ';' ';' ';' ';'X';' ';'X';' ';' ';' ']
+            [' ';' ';' ';' ';'X';' ';' ';' ';'X';' ']
+            [' ';' ';' ';' ';' ';' ';'X';'X';'X';' ']
             [' ';' ';' ';' ';' ';' ';'X';' ';' ';' ']
             [' ';' ';' ';' ';' ';' ';'X';' ';' ';' ']
-            [' ';' ';' ';' ';'X';'X';'X';' ';' ';' ']
-            [' ';' ';' ';' ';'X';' ';' ';' ';' ';' ']
-            [' ';' ';' ';' ';'X';' ';' ';' ';' ';' ']
-            [' ';' ';' ';' ';'X';' ';' ';' ';' ';' ']
-            [' ';' ';' ';' ';'X';' ';' ';' ';' ';' ']
-            [' ';' ';' ';' ';'X';' ';' ';' ';' ';' ']
-            [' ';' ';' ';' ';' ';' ';' ';' ';' ';' ']
-            [' ';' ';' ';' ';' ';' ';' ';' ';' ';' ']
-            [' ';' ';' ';' ';' ';' ';' ';' ';' ';' ']
         ]
-    
+    let width, height = List.length testArray.[0], List.length testArray
+
     let start = 0, 0
-    let goal = List.length testArray.[0] - 1, List.length testArray - 1
+    let goal = width - 1, height - 1
 
     let blocks = 
         testArray 
@@ -32,15 +33,26 @@ let main _ =
                 |> List.map (fun (pos, _) -> pos))
         |> List.concat |> Set.ofList
     let neighbours (x, y) =
-        let found = [-1..1] |> List.collect (fun nx -> [-1..1] |> List.filter (fun ny -> ny <> nx || ny <> 0) |> List.map (fun ny -> x + nx, y + ny))
-        found |> Seq.filter (fun n -> not <| Set.contains n blocks)
+        let found = 
+            [-1..1] |> List.collect (fun nx ->
+            [-1..1] |> List.filter (fun ny -> ny <> nx || ny <> 0) |> List.map (fun ny -> x + nx, y + ny))
+        found |> Seq.filter (fun (nx, ny) -> 
+            nx > 0 && ny > 0 &&
+            nx < width && ny < height &&
+            not <| Set.contains (nx, ny) blocks)
 
     let gScore _ _ = 1.
     let fScore (x, y) (gx, gy) = 
         sqrt ((float gx - float x)**2. + (float gy - float y)**2.)
 
     match AStar.search start goal { neighbours = neighbours; gCost = gScore; fCost = fScore } with
-    | Some path -> printf "Success!"
+    | Some path -> 
+        printf "Success! Solution:"
+        testArray |> List.mapi (fun y row -> 
+            row 
+            |> List.mapi (fun x cell -> if Seq.contains (x, y) path then "#" else cell.ToString())
+            |> String.concat ""
+        ) |> List.iter (printfn "%s")
     | None -> printf "No Path Found"
 
     0
