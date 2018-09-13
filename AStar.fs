407f742f8968b1716ef0715227abbcce397bdda8
Christopher <chrispritchard.nz@gmail.com>
2018-09-14 08:23:50 +1200

    Start of method signatures

--- AStar.fs
+++ AStar.fs
@@ -1,2 +1,14 @@
 module AStar
 
+type Config<'a> = 
+    {
+        
+        neighbours: 'a -> seq<'a>
+        gCost: 'a -> 'a -> float
+        hCost: 'a -> 'a -> float
+    }
+
+let search<'a> start goal config =
+    seq {
+        yield start
+    }
